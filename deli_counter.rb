5dae58af77fe05bc8dc21462f7d8319f00590bfa
Elijah Kirschen <ekirschen584@gmail.com>
2020-05-31 23:18:36 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -19,7 +19,7 @@ end
 def take_a_number(array, name)
   # binding.pry 
   array << name
-  puts "Welcome, #{name}.You are number #{array.length} in line."
+  puts "Welcome, #{name}. You are number #{array.length} in line."
 end
 
 def now_serving(array)
